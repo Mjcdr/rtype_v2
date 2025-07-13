9a5f6336e4bbfdc300501e5b4df33b8501b7ccf7
mjcdr <arctic.iguana.327@gmail.com>
2025-07-13 23:39:54 +0200

    Game Over scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public string gameOverSceneName = "GameOver";
+    public string nextLevelSceneName = "BossLevel";
+
+    private PlayerController player;
+    private EnemyBehavior[] enemies;
+
+    void Start()
+    {
+        player = Object.FindFirstObjectByType<PlayerController>();
+    }
+
+    void Update()
+    {
+        // Controlla se il player è stato distrutto
+        if (player == null)
+        {
+            SceneManager.LoadScene(gameOverSceneName);
+            return;
+        }
+
+        // Controlla se ci sono nemici attivi
+        enemies = Object.FindObjectsByType<EnemyBehavior>(FindObjectsSortMode.None);
+        if (enemies.Length == 0)
+        {
+            SceneManager.LoadScene(nextLevelSceneName);
+        }
+    }
+}
